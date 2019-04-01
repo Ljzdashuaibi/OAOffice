b0541e8995c59a5833a0aa6e5c3b7fc48682b83b
Ljzdashuaibi <49134810+Ljzdashuaibi@users.noreply.github.com>
2019-04-01 10:21:14 +0800

    Update User.java
    1

--- src/cn/oa/pojo/User.java
+++ src/cn/oa/pojo/User.java
@@ -3,7 +3,7 @@ package cn.oa.pojo;
 import java.util.Date;
 
 public class User {
-	//李金钊大傻逼11111
+	//李金钊大傻逼2222222222
 	private Integer id;
     private String account;//登录名
     private String password;
